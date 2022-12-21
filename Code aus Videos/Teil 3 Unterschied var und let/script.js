a51b052586d2598c4ae87286456b8dac538de4ae
Talk2Studios <121163565+Talk2Studios@users.noreply.github.com>
2022-12-21 11:20:05 +0100

    Add files via upload

---
+++ Code aus Videos/Teil 3 Unterschied var und let/script.js
@@ -0,0 +1,19 @@
+//funktiosnname: test1
+//funktiosnname: test
+//funktiosnname: test2
+
+// Was sind das für Elemente?
+// was ist der Unterschied zwischen var und let?
+var blau = 8
+function test1() {
+    console.log(blau)
+}
+function test() {
+    blau = 3
+    let rot = 6
+    console.log(rot)
+}
+console.log(rot)
+function test2() {
+console.log(blau)
+}
